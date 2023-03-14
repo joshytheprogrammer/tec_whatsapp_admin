b0310f42bf2b6f786a2574bd379084da65e7ba57
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-14 15:52:45 +0100

    made changes

---
+++ pages/create/banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="banner">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from "~/components/Create/BannerForm.vue"
+export default {
+  components: {
+    Form,
+  }
+}
+</script>
